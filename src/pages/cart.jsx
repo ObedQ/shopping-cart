4a6608e5b8c7c15e9b9472e455b513e6b54a5a94
ObedQ <olotu@st.ug.edu.gh>
2024-03-22 09:51:18 +0000

    pages added

--- src/pages/cart.jsx
+++ src/pages/cart.jsx
@@ -1,11 +1,18 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
 
 const Cart = () => {
-  return (
-    <div>
-      Cart
-    </div>
-  )
-}
+  const [totalCart, setTotalCart] = useState(0);
 
-export default Cart
+  const { cart } = useSelector((state) => state);
+
+  useEffect(() => {
+    setTotalCart(cart.reduce((acc, curr) => acc + curr.price, 0));
+  }, [cart]);
+
+  console.log(cart);
+
+  return <div>Cart</div>;
+};
+
+export default Cart;
